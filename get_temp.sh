03f0eb424fc26112da22cc3df4056ad497ba0d1c
burm.leonid@gmail.com <burmist-git>
2024-12-19 18:30:42 +0100

    remove an old name

--- get_temp.sh
+++ get_temp.sh
@@ -6,7 +6,7 @@ echo "time_unix,RES" | tee -a $file_out
 for i in $(seq 1 $n_meas)
 do
     time_unix=$(date +%s)
-    RES=$(sudo python3 agilent_34410A_t.py file /dev/usbtmc0 | grep RES: | awk '{print $2}')
+    RES=$(sudo python3 agilent_34410A.py file /dev/usbtmc0 | grep RES: | awk '{print $2}')
     echo "$time_unix,$RES" | tee -a $file_out
     sleep 1
 done
